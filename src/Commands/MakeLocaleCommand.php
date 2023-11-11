8ae6a9731988aab2790ca0ce07d86e50039204de
Cesar Mendez <acmined@gmail.com>
2023-11-11 00:18:18 -0400

    fix: use new lang path fix: create localization files
    ui: add better color for cli output

--- src/Commands/MakeLocaleCommand.php
+++ src/Commands/MakeLocaleCommand.php
@@ -26,32 +26,39 @@ final class MakeLocaleCommand extends Command implements PromptsForMissingInput
             return;
         }
 
-        $this->line('Creating localization files for: '.$name);
+        $this->line("<options=reverse;fg=white>Creating localization files for: {$name}</>\n");
 
         $file_name      = end($locale_parts);
         $directory_path = implode('/', array_slice($locale_parts, 0, -1));
 
         $this->createLocales($file_name, $directory_path, array_keys(config('app.available_locales', [])));
+
+        $this->line("<options=reverse;fg=white>Hasta luego! 👋</>\n");
     }
 
     private function createLocales($file_name, $directory_path, $locales)
     {
-        $base_path = resource_path('lang/');
+        $base_path = base_path('lang/');
 
         foreach ($locales as $locale) {
             $locale_path = $base_path.$locale.'/'.$directory_path;
-            if (!File::isDirectory(dirname($locale_path))) {
-                File::makeDirectory(dirname($locale_path), 0777, true, true);
-            }
 
             $file_path = $locale_path.'/'.$file_name.'.php';
-
-            if (!File::exists($file_path)) {
-                File::put($file_path, "<?php\n\nreturn [];\n");
-
-                $sub_path = $locale.'/'.$directory_path.'/'.$file_name;
-                $this->line("<options=bold;fg=green> {$sub_path} CREATED </> 🌎🌍🌏🎉\n");
+            if (!File::isDirectory(dirname($file_path))) {
+                File::makeDirectory(dirname($file_path), 0777, true, true);
             }
+
+            $sub_path = $locale.'/'.$directory_path.'/'.$file_name;
+
+            if (File::exists($file_path)) {
+                $this->line("<options=bold;fg=yellow> {$sub_path} already exists, skiping...</>\n");
+
+                continue;
+            }
+
+            File::put($file_path, "<?php\n\nreturn [];\n");
+
+            $this->line("<options=bold;fg=green> CREATED 🌎🌍🌏 {$sub_path} 🎉</>\n");
         }
     }
 }
